dd7234af367c6fbeca4d55fbb5f9a5c76e14f21b
Jari Sokka <sokkajar@gmail.com>
2022-07-18 12:57:47 +0300

    Basic layout for the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-
+import Header from "./components/header/Header"
+import Landing from "./components/landing/Landing"
+import Recent from "./components/recent/Recent"
+import Gallery from "./components/gallery/Gallery"
+import About from "./components/about/About"
+import Contact from "./components/contact/Contact"
+import Footer from "./components/footer/Footer"
 
 const App = () => {
   return (
     <div className="App">
-      <p>Hello</p>
+      <Header />
+      <Landing />
+      <Recent />
+      <Gallery />
+      <About />
+      <Contact />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <section>
+      <div>
+        <h2>Footer</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu metus sed sem malesuada aliquam ac nec risus. Proin id odio nec ligula bibendum consequat. In est elit, tristique non mauris tempor, finibus fringilla mi. Pellentesque blandit ac mauris eu volutpat. In lobortis nisl vel congue facilisis. Pellentesque ultricies turpis leo, et consectetur massa dictum non. Vivamus mattis commodo elit, in hendrerit dolor mattis sit amet. Sed iaculis diam a rhoncus volutpat. Integer vel varius augue. Maecenas sollicitudin, est in finibus semper, justo enim malesuada lectus, at dictum nibh enim et urna. Fusce pulvinar enim id arcu varius dignissim. Praesent vel felis lectus. Nullam volutpat quam mi, non convallis quam mattis aliquet.</p>
+      </div>
+    </section>
+  )
+}
+
+export default Footer
