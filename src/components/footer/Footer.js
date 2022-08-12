116270ad966ba78a47de33a74a32e9c10b1aa88e
Jari Sokka <sokkajar@gmail.com>
2022-08-12 13:17:05 +0300

    Add footer and start of pipeline

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,20 @@
 import './Footer.css'
+import {BsInstagram, BsFacebook} from 'react-icons/bs'
+import {Fa500Px} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <section>
-      <div className='container'>
-        <h2>Footer</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu metus sed sem malesuada aliquam ac nec risus. Proin id odio nec ligula bibendum consequat. In est elit, tristique non mauris tempor, finibus fringilla mi. Pellentesque blandit ac mauris eu volutpat. In lobortis nisl vel congue facilisis. Pellentesque ultricies turpis leo, et consectetur massa dictum non. Vivamus mattis commodo elit, in hendrerit dolor mattis sit amet. Sed iaculis diam a rhoncus volutpat. Integer vel varius augue. Maecenas sollicitudin, est in finibus semper, justo enim malesuada lectus, at dictum nibh enim et urna. Fusce pulvinar enim id arcu varius dignissim. Praesent vel felis lectus. Nullam volutpat quam mi, non convallis quam mattis aliquet.</p>
+    <footer>
+      <div className='footer-socials'>
+        <a href='https://instagram.com' target='_blank' rel="noreferrer"><BsInstagram/></a>
+        <a href='https://500px.com' target='_blank' rel="noreferrer"><Fa500Px/></a>
+        <a href='https://facebook.com' target='_blank' rel="noreferrer"><BsFacebook/></a>
       </div>
-    </section>
+      <div className='copyright'>
+        <small>All material &copy; copyright by Jari Sokka. All rights recerved.</small>
+      </div>
+      <div className='the-end'></div>
+    </footer>
   )
 }
 
